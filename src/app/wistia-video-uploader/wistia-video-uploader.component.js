c4b1be5443271b37194de141cec2d03651ea4d69
Mauro Fruet <mauro.fruet@gpi.it>
2018-06-10 14:11:59 +0200

    add form to upload a video to wistia

--- src/app/wistia-video-uploader/wistia-video-uploader.component.js
+++ src/app/wistia-video-uploader/wistia-video-uploader.component.js
@@ -7,20 +7,55 @@
     controllerAs: "$ctrl"
   });
 
-  function WistiaVideoUploaderController() {
+  WistiaVideoUploaderController.$inject = ["$element", "WistiaVideoUploaderCostants"];
+  function WistiaVideoUploaderController($element, WistiaVideoUploaderCostants) {
     var $ctrl = this;
 
-    $ctrl.$onInit = function() {
-      $("#fileupload").fileupload({
-        dataType: "json",
-        done: function(e, data) {
-          $.each(data.result.files, function(index, file) {
-            $("<p/>")
-              .text(file.name)
-              .appendTo(document.body);
-          });
-        }
-      });
-    };
+    $ctrl.$onInit = onInit;
+    $ctrl.isUploaded = isUploaded;
+    $ctrl.getVideoClass = getVideoClass;
+
+    var uploaded = false;
+    var videoId = undefined;
+
+    function onInit() {
+      var form = $element.find("form");
+
+      form
+        .fileupload({
+          type: "POST",
+          url: WistiaVideoUploaderCostants.URL,
+          done: onAdded,
+          fail: function(e, data) {
+            console.log("error", data);
+          },
+          progress: function(e, data) {
+            var progress = parseInt((data.loaded / data.total) * 100, 10);
+            console.log(data.loaded, data.total);
+            $(".progress .progress-bar").css("width", progress + "%");
+          }
+        })
+        .on("fileuploadadd", function(e, data) {
+          var file = data.files[0];
+          var data = new FormData();
+          data.append("file", file);
+          data.append("api_password", WistiaVideoUploaderCostants.API_PASSWORD);
+          $(this).fileupload("option", "data", data);
+        });
+    }
+
+    function isUploaded() {
+      return uploaded;
+    }
+
+    function getVideoClass() {
+      return "video wistia_embed wistia_async_" + videoId;
+    }
+
+    function onAdded(e, data) {
+      uploaded = true;
+      console.log("added");
+      videoId = data.result["hashed_id"];
+    }
   }
 })();
---
+++ src/app/wistia-video-uploader/wistia-video-uploader.constant.js
@@ -0,0 +1,7 @@
+(function() {
+  "use strict";
+  angular.module("app").constant("WistiaVideoUploaderCostants", {
+    API_PASSWORD: "put here your Wistia API password",
+    URL: "https://upload.wistia.com"
+  });
+})();
